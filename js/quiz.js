4507dec75ca85452754f8ee89ba1b45120ee78ab
Gus Wezerek <gwezerek@gmail.com>
2014-04-08 14:27:56 -0400

    API hookup in place.

--- js/quiz.js
+++ js/quiz.js
@@ -29,22 +29,101 @@ d3.csv(spreadsheetURL, function(error, myData) {
 
 
 
+
 // HELPERS
 // =============================================
-function populateIngredients(data) {
+function getIngredients(data) {
+
+	var ingredients = [];
+	
+	for (var i = 0; i < 4; i++) {
+		var randomInt = getRandomInt(0, data.length);
+		var ingredient = DATA[randomInt];
+		fillJSON(ingredient, ingredients);
+	};
+}
+
+function fillJSON(ingredient, ingredients) {
+
+	var ingredientName = ingredient["ingredient"];
+
+	var url = "http://api.yummly.com/v1/api/recipes?_app_id=d4056344&_app_key=cf30fed394ef2013d82d03179ca4f961&q=" + ingredientName + "&requirePictures=true";
+
+	$.ajax({
+	    type: 'GET',
+	    url: url,
+	    dataType: 'JSONP',
+	    jsonp: 'callback',
+	    success: function(json) {
+	    	var matches = json["matches"];
+	    	if (matches.length > 0) {
+	    		console.log(matches);
+		    	getRecipe1(matches, ingredient, ingredients);
+	    	}
+	    }
+	});
+}
+
+function getRecipe1(matches, ingredient, ingredients){
+
+	var url = "http://api.yummly.com/v1/api/recipe/" + matches[0]["id"] +"?_app_id=d4056344&_app_key=cf30fed394ef2013d82d03179ca4f961";
+
+	var promise = $.ajax({
+	    type: 'GET',
+	    url: url,
+	    dataType: 'JSONP',
+	    jsonp: 'callback',
+	    success: function(recipe) {
+	    	ingredient["image_url"] = recipe["images"][0]["hostedLargeUrl"];
+	    	ingredient["recipe1_name"] = recipe["name"];
+	    	ingredient["recipe1_source"] = recipe["source"]["sourceDisplayName"];
+	    	ingredient["recipe1_url"] = recipe["source"]["sourceRecipeUrl"];
+	    	if (matches.length > 1) {
+		    	getRecipe2(matches[1]["id"], ingredient, ingredients)
+		    }
+	    }
+	});
+}
+
+function getRecipe2(id, ingredient, ingredients){
+
+	var url = "http://api.yummly.com/v1/api/recipe/" + id +"?_app_id=d4056344&_app_key=cf30fed394ef2013d82d03179ca4f961";
+
+	var promise = $.ajax({
+	    type: 'GET',
+	    url: url,
+	    dataType: 'JSONP',
+	    jsonp: 'callback',
+	    success: function(recipe) {
+	    	ingredient["recipe2_name"] = recipe["name"];
+	    	ingredient["recipe2_source"] = recipe["source"]["sourceDisplayName"];
+	    	ingredient["recipe2_url"] = recipe["source"]["sourceRecipeUrl"];
+
+	    	pushUpdatedIngredient(ingredients, ingredient);
+	    }
+	});
+
+	promise.done(function(){
+		if (ingredients.length === 4) {
+			populateIngredients(ingredients);
+		}
+	});
+}
+
+function populateIngredients(selectedIngredients) {
+
 	var myObj = {};
 	var toAppendString = "";
 
-	// Get the four ingredients
-	var ingredientsData = getRandos(DATA);
-
 	// Create objects that underscore likes
-	myObj["ingredients"] = ingredientsData;
-	ingredientsData = myObj;
+	myObj["ingredients"] = selectedIngredients;
+	selectedIngredients = myObj;
 	
 	// Compile the list
-	for (i = 0; i < ingredientsData.ingredients.length; i++) {
-		toAppendString += ingredientsTemplate(ingredientsData.ingredients[i]);
+	for (i = 0; i < selectedIngredients.ingredients.length; i++) {
+		var selected = selectedIngredients.ingredients[i];
+		selected["temp_index"] = i +1;
+		toAppendString += ingredientsTemplate(selected);
 	}
 
 	// Fade out current list 
@@ -52,9 +131,6 @@ function populateIngredients(data) {
 
 	// Append the list
 	$(".viz-ingredients").html(toAppendString);
-
-	// Fade out Recipes
-
 
 	// Reveal the ingredients one by one
 	(function showIngredient (i) {          
@@ -68,8 +144,6 @@ function populateIngredients(data) {
 			}
 		}, 500)
 	})(0);
-
-
 }
 
 function hideRecipes() {
@@ -80,45 +154,11 @@ function showRecipes() {
 	$(".viz-subhead-recipes").fadeIn(200);
 }
 
-function getRandos(data) {
-	var ingredients = [];
-
-	for (var i = 0; i < 4; i++) {
-		var randomInt = getRandomInt(0, data.length);
-		var ingredient = DATA[randomInt];
-		ingredient["temp_index"] = i +1;
-		fillJSON(ingredient["ingredient"]);
-		ingredients.push(ingredient);
-	};
-
-	return ingredients;
+function pushUpdatedIngredient(ingredientsArray, ingredient) {
+	ingredientsArray.push(ingredient);
 }
 
-function fillJSON(ingredient) {
 
-	var url = "http://api.yummly.com/v1/api/recipes?_app_id=d4056344&_app_key=cf30fed394ef2013d82d03179ca4f961&q=" + ingredient;
-
-	$.ajax({
-	    type: 'GET',
-	    url: url,
-	    dataType: 'JSONP',
-	    jsonp: 'callback',
-	    success: function(json) {
-	    	console.log("success");
-	    	console.log(ingredient);
-	    	console.log(json);
-	    },
-	    error: function(e) {
-	    	console.log("fail");
-	    	console.log(ingredient);
-	    }
-	});
-
-	// function myFunc(data) {
-	// 	console.log(data);
-	// }
-
-}
 
 
 
@@ -128,7 +168,7 @@ function fillJSON(ingredient) {
 // HANDLERS
 // =============================================
 $(".viz-basket").on("click", function() {
-	populateIngredients(DATA);
+	getIngredients(DATA);
 });
 
 // $(".viz-ingredients").on("click", ".viz-ingredient-header", function() {
